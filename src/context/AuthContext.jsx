1d68d0567ee793ebb13c362217b53b08df311dbb
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-12-28 18:54:54 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,11 @@
 import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
-
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+ const [isAuth, setIsAuth] = useState(false);
+ return (
+<AuthContext.Provider value={{ isAuth, setIsAuth }}>
+     {children}
+</AuthContext.Provider>
+ );
+};
 export const useAuth = () => useContext(AuthContext);
